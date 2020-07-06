d426e0880aeb6ebf139885fcb1064fbd8bece93e
haerang <gofkdvjvl@naver.com>
2020-07-06 17:18:41 +0900

    express 기본 환경 세팅

---
+++ bamin-13/modules/responseMessage.js
@@ -0,0 +1,29 @@
+module.exports = {
+    NULL_VALUE: "필요한 값이 없습니다",
+    OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+    DB_ERROR: "데이터베이스 오류",
+  
+    // 회원가입
+    CREATED_USER: "회원 가입 성공",
+    DELETE_USER: "회원 탈퇴 성공",
+    ALREADY_ID: "이미 사용중인 아이디입니다.",
+  
+    // 로그인
+    LOGIN_SUCCESS: "로그인 성공",
+    LOGIN_FAIL: "로그인 실패",
+    NO_USER: "존재하지 않는 회원입니다.",
+    MISS_MATCH_PW: "비밀번호가 맞지 않습니다.",
+  
+  
+    // 뮤직리스트 조회
+    NO_MUSICLIST: "리스트가 없습니다.",
+    SHOW_MUSICLIST_SUCCESS: "뮤직리스트 조회 성공",
+  
+    // 댓글 조회
+    NO_COMMENTS_DATA: "작성된 댓글이 없습니다.",
+    COMMENTS_SUCCESS: "댓글 조회 성공",
+  
+    // 좋아요
+    SUCCESS_UPDATE_LIKE: "음악 좋아요 업데이트 성공",
+    FAIL_UPDATE_LIKE: "음악 좋아요 업데이트 실패"
+  };
