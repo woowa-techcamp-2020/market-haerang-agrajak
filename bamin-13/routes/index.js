a8b69d418ac6c92c0ce0f6f8d5e3eee161b1bbe1
agrajak <korbots@gmail.com>
2020-07-09 14:08:00 +0900

    사용하지 않는 라우터 제거

--- bamin-13/routes/index.js
+++ bamin-13/routes/index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const loginRouter = require('./login');
 const signupRouter = require('./signup');
 const completeRouter = require('./complete');
-const testRouter=require('./test');
 
 const router = express.Router();
 
 router.use('/login', loginRouter);
 router.use('/signup', signupRouter);
 router.use('/complete', completeRouter);
-router.use('/test',testRouter);
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {  
   res.render('main');
 });
 
