cbc18e55f9259d95ed920d16ea0ad77b18e340bc
haerang <gofkdvjvl@naver.com>
2020-07-09 15:02:54 +0900

    새로운 회원가입 페이지와 새로운 기능, 마크업 연결

--- bamin-13/public/scripts/signup.js
+++ bamin-13/public/scripts/signup.js
@@ -1,18 +1,30 @@
 const checkList = ['id', 'password', 'name','password-chk'];
 
 const formElement = document.querySelector('form');
+
+// 인증 버튼 클릭시 모달 관련 변수들
+const authBtn = document.getElementById('phone-auth-btn');
+const openPostalButton = document.querySelector('#address-btn')
+const numberInputText = document.querySelector('#auth-number-input');
+const numberAlert = document.getElementById('number-alert');
+const blueBtn=document.querySelectorAll('.item-2-btn');
 const modal = document.querySelector('.modal-container');
 const closeModalBtn = document.getElementById('close-modal-btn')
 const modalOverlay = document.querySelector('.modal-overlay');
-const authBtn = document.getElementById('auth-btn');
-const openPostalButton = document.querySelector('#address-btn')
-const codeBox = document.querySelector('#code-box');
-const numberInputText = document.querySelector('#auth-number-input');
-const numberAlert = document.getElementById('number-alert');
+
+
+//주소 찾기
+const addressBtn=document.querySelector('#address-btn');
 const addressCheckBox = document.getElementById('select-info-chk')
+const codeBox = document.querySelector('#code-box');
+
+// 약관 동의
 const essentialCheckBox = document.getElementById('essential-info-chk')
 const termsAgreeCheckBox = document.getElementById('terms-agree-chk')
 const advertiseCheckBox = document.getElementById('advertise-info-chk')
+
+
+//우편 번호 찾기 기능 
 const postalBox = new daum.Postcode({
     oncomplete: function(data) {
         const {zonecode, address} = data;
@@ -25,9 +37,21 @@ openPostalButton.addEventListener('click', ()=>{
     postalBox.open();
 })
 
+
+// 주소 선택 여부 따른 입력 가능/불가 기능 
 function setAddressBoxEnabled(value){
+
+    if(value){
+        addressBtn.classList.remove('item-2-btn');
+        addressBtn.classList.add('blue-btn');
+    }else{
+        addressBtn.classList.add('item-2-btn');
+        addressBtn.classList.remove('blue-btn');
+    }
+
     const addressBox = document.getElementById('postcode-box')
     addressBox.childNodes.forEach(node=>{
+        node.disabled=!value;
         node.childNodes.forEach(elem=>{
             elem.disabled = !value
         })
@@ -39,6 +63,8 @@ addressCheckBox.addEventListener('input', (event)=>{
     const {checked} = event.target
     setAddressBoxEnabled(checked);
 })
+
+// 전체 약관 동의 기능 
 termsAgreeCheckBox.addEventListener('input', (event)=>{
     const {checked} = event.target
     essentialCheckBox.checked = checked
@@ -71,8 +97,8 @@ formElement.addEventListener('focusout', (event)=>{
     const alertElement = document.getElementById(target.id+'-alert');
     if(!validation){
         // 만약 유효성 검사에 실패했다면 경고문을 보여주고 빨간 테투리 표시를 해준다
-        if(!alertElement.classList.contains('is-visible')){
-            alertElement.classList.add('is-visible');
+        if(!alertElement.classList.contains('visible')){
+            alertElement.classList.add('visible');
         }
         if(!actualElement.classList.contains('red-box')){
             actualElement.classList.add('red-box');
@@ -80,8 +106,8 @@ formElement.addEventListener('focusout', (event)=>{
     }
     else {
         // 유효성 검사에 성공했다면 경고문을 숨기고 빨간 테두리 표시를 없애준다.
-        if(alertElement.classList.contains('is-visible')){
-            alertElement.classList.remove('is-visible');
+        if(alertElement.classList.contains('visible')){
+            alertElement.classList.remove('visible');
         }
         if(actualElement.classList.contains('red-box')){
             actualElement.classList.remove('red-box');
@@ -106,18 +132,24 @@ function renderCountDown(){
     }
     else {
         timer.innerText = ''
+        numberAlert.innerText='입력시간을 초과하였습니다'
     }
 }
 
 
 function requestAuthCode(){
+    //todo: 인증이 완료되었을 경우에는 그냥 리턴하는 함수
+    // if(true){
+    //     //remove eventlistener
+    //     return;
+    // }
     const http = new XMLHttpRequest();
     const phone = document.getElementById('phone').value;
     if(!http) return;
     http.onload = function(){
         if(http.status === 200 || http.status == 201){
             const {authCode, invalidAt} = JSON.parse(http.responseText)['data']
-            openModal(authCode);
+            openModal(authCode); //모달 열기
             countFinishedAt = invalidAt;
         }
     };
@@ -125,6 +157,9 @@ function requestAuthCode(){
     http.setRequestHeader('Content-Type', 'application/json');
     http.send(JSON.stringify({phone}));
 }
+
+// 인증번호 생성 관련 모달 함수
+
 authBtn.addEventListener('click', requestAuthCode);
 
 const openModal=(value)=>{
@@ -135,11 +170,47 @@ const openModal=(value)=>{
 const closeModal=()=>{
     modal.classList.add('hidden');
     codeBox.classList.remove('hidden');
-    numberAlert.classList.add('is-visible');
+    numberAlert.classList.remove('hidden');
     numberInputText.classList.add('red-box');
+    blueBtn.forEach(v=>{
+        if(!v.classList.contains('address-btn')){
+            v.classList.remove('item-2-btn');
+            v.classList.add('blue-btn');
+            authBtn.value='재전송';
+        }
+    })
     renderCountDown()
 }
 
-//authBtn.addEventListener('click',openModal);
 closeModalBtn.addEventListener('click',closeModal);
 modalOverlay.addEventListener('click',closeModal);
+
+
+// 파란색 버튼 클릭시 원래 스타일로 돌아옴 
+
+// blueBtn.forEach(v=>{
+//     if(!v.classList.contains('address-btn')){
+//         v.classList.remove('item-2-btn');
+//         v.classList.add('blue-btn');
+//         authBtn.value='재전송';
+//     }
+// })
+
+
+
+// todo: 인증번호 확인 후 메세지, 버튼 
+
+function authComplete(){
+    if(true){// true 대신 인증번호 확인하는 함수 필요
+
+        blueBtn.forEach(v=>{
+            v.addEventListener('click',()=>{
+                v.classList.remove('blue-btn');
+                v.classList.add('item-2-btn');
+            })
+        })
+
+    }
+
+}
+authComplete();
