d661e0d02bcd2c40bea206be43606934a694c8ed
haerang <gofkdvjvl@naver.com>
2020-07-09 16:29:58 +0900

    인증파트 합치기 완료
    login.js에서 사용되던 인증부분 코드를 utils.js로 옮기고 회원가입 페이지에도 일괄 적용

--- bamin-13/public/scripts/utils.js
+++ bamin-13/public/scripts/utils.js
@@ -13,12 +13,92 @@ function validatePassword(password){
   return /^([A-Za-z0-9]+)$/.test(password)
 }
 
-function validatePasswordChk(password,passwordChk){
-  if(password!==passwordChk)return false;
+function validatePasswordChk(password, passwordChk){
+  if(password !== passwordChk) return false;
   return true;
 }
 
+const form = document.forms[0]
+const elements = form.elements
 
+form.addEventListener('focusout', validateForms);
+
+function validateQueue(queue){
+    // queue(배열)에 있는 요소들이 모두 조건에 부합하는가를 검사한다.
+    for(const elem of queue){
+        const alertTarget = elem.getAttribute('alert');
+        // html에 alert attribute가 있는 htmlElement만 검사한다.
+        if(alertTarget){
+            const alertElem = document.getElementById(alertTarget);
+            const {validation, message} = validate(elem);
+            if(!validation){
+                elem.classList.add('red-box')
+                alertElem.classList.remove('hidden')
+                alertElem.innerText = message
+                // 이벤트에서 불러오지 않았을 경우에만 focus를 한다.
+                if(queue.length > 1){ 
+                    elem.focus();
+                }
+                return false;
+            }
+            elem.classList.remove('red-box')
+            alertElem.classList.add('hidden')
+            alertElem.innerText = ''
+        }
+    }
+    return true;
+}
+function validate(elem){
+    const {value, id, checked=true} = elem;    
+    let message = '';
+
+    if(id === 'id'){
+        if(value.length == 0) message = '아이디를 입력해주세요';
+        else if(!validateId(value)) message = '아이디는 4-20자의 영소문자, 숫자, 특수기호(-, _)만 사용가능합니다.'
+    }
+    if(id == 'password'){
+        if(value.length == 0) message = '비밀번호를 입력해주세요';
+        else if(!validatePassword(value)) message = '비밀번호는 영문과 숫자를 포함하여 8~20자로 입력해주세요.'
+    }
+    if(id == 'password-chk'){
+        if(value.length == 0) message = '확인 비밀번호를 입력해주세요';
+        else if(!validatePasswordChk(value, elements['password'].value)) message = '비밀번호와 일치하지 않습니다. 다시 입력해주세요.'
+    }
+    if(id == 'email-front'){
+        if(value.length == 0) message = '이메일 아이디를 입력해주세요';
+    }
+    if(id == 'email-back'){
+      if(value.length == 0) message = '이메일 도메인을 입력해주세요';
+    }
+    if(id == 'name'){
+      if(value.length == 0) message = '이름을 입력해주세요'
+      else if(!validateName(value)) message = '이름은 영어와 한글만 사용가능합니다.'
+    }
+    if(id == 'phone'){
+      if(value.length == 0) message = '핸드폰 번호를 입력해주세요'
+      // TODO: validatePhone() 구현
+    }
+    if(id == 'auth-number-input'){
+      if(value.length == 0) message = '인증번호를 입력해주세요.'
+    }
+    if(id == 'essential-info-chk'){
+      if(!checked) message = '필수항목에는 체크를 하셔야합니다.'
+    }
+    return {
+        validation: message === '',
+        message
+    }
+}
+function validateForms(event){
+    if(event){
+        // 이벤트에서 이 함수가 호출된 경우에는 큐에 이벤트 타겟 하나만 넣는다.
+        return validateQueue([event.target])
+    }
+    else {
+        // 이벤트에서 이 함수가 호출된 경우에는 큐에 폼에 있는 모든 인풋을 넣는다.
+        return validateQueue([...elements])
+    }
+}
 
 
 /*
