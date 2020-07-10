0dfadd2252b6ec7b826e5791aeb4056c4018d8b2
haerang <gofkdvjvl@naver.com>
2020-07-10 12:31:27 +0900

    모달 추가 및 디자인 수정

--- bamin-13/public/scripts/detailModal.js
+++ bamin-13/public/scripts/detailModal.js
@@ -1,21 +1,14 @@
 
 
-const openModal2=(value)=>{
-    modal.classList.remove('hidden');
-    document.querySelector('.modal-content').innerHTML=
-    `
-    <div style="text-align:left">
-    <p style="padding:10px">
-    정보통신망 이용촉진 및 정보보호 등에 관한 법률에서는 만 14세 미만 아동의 개인정보 수집 시 법정대리인 동의를 받도록 규정하고 있으며, 만 14세 미만 아동이 법정대리인 동의없이 회원가입을 하는 경우 회원탈퇴 또는 서비스 이용이 제한 될 수 있습니다.</p></div>
-    <input id="close-modal-btn" value="확인" />
-    `
+const openDetailModal=(value)=>{
+    document.querySelector('#detail-modal').classList.remove('hidden');
 }
 
-const closeModal2=()=>{
-    modal.classList.add('hidden');
+const closeDetailModal=()=>{
+    document.querySelector('#detail-modal').classList.add('hidden');
 
 }
 
-document.querySelector('#age-detail').addEventListener('click',openModal2);
-document.querySelector('#close-modal-btn').addEventListener('click',closeModal2);
-modalOverlay.addEventListener('click',closeModal2);
+document.querySelector('#age-detail').addEventListener('click',openDetailModal);
+document.querySelector('#close-detail-modal-btn').addEventListener('click',closeDetailModal);
+modalOverlay.addEventListener('click',closeDetailModal);
